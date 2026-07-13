f736cef63fd3053f453cd8333deb6a93edb58251
leedn30318 <sudo2630318@sonline20.sen.go.kr>
2026-07-13 10:40:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 ﻿import streamlit as st
  
 st.title("부엉이가 수영할 때 나는 소리는?")
-st.write("첨부엉 첨부엉") #st: 스트립닛
+st.write("첨부엉 첨부엉")
+
+﻿지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
+st.write("당신이 고른 지역:", 지역)
