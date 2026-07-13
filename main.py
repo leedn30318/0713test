9fc8f5bd2ea96efb62209c28b9d86e84f96ba460
leedn30318 <sudo2630318@sonline20.sen.go.kr>
2026-07-13 10:48:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 st.title("부엉이가 수영할 때 나는 소리는?")
 st.write("첨부엉 첨부엉")
 
-﻿﻿﻿지역 = st.selectbox("지역을 골라 보세요", ["서울", "양평", "부산"])
-st.write("당신이 고른 지역:", 지역)
+﻿﻿﻿숫자 = st.slider("좋아하는 숫자", 0, 100)
+st.write("고른 숫자:", 숫자)
+ 
+if st.button("풍선 날리기"):
+	st.balloons()
